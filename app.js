854ef3ccb8778936426ab4457f30a3fcb34c2e6f
namantia1@gmail.com <namantia1@gmail.com>
2021-01-26 14:14:03 -0500

    3 second error when there is no user input

--- app.js
+++ app.js
@@ -24,8 +24,37 @@ function calculateResults(e){
         totalPayment.value = (monthly * calculatedPayents).toFixed(2);
         totalInterest.value = ((monthly * calculatedPayents)-principal).toFixed(2);
     } else {
-        console.log('Please check your numbers');
+        showError('Please check your numbers');
     }
 
     e.preventDefault();
+}
+
+// Show Error
+function showError(error){
+    // Create a div
+    const errorDiv = document.createElement('div');
+
+    // Get elements
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    // Add class
+    errorDiv.className = 'alert alert-danger';
+
+    // Create text node and append to div
+
+    errorDiv.appendChild(document.createTextNode(error));
+
+    // Insert error above heading
+    card.insertBefore(errorDiv, heading);
+
+    // Clear error afer 3 seconds
+
+    setTimeout(clearError, 3000);
+}
+
+//Clear error
+function clearError(){
+    document.querySelector('.alert').remove();
 }
