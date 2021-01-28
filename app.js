2365e1bdd0df9b13a6f6d1cb9a7e0fbc425c76bc
namantia1@gmail.com <namantia1@gmail.com>
2021-01-28 15:37:14 -0500

    added a loader gif while app is calculating loan amount

--- app.js
+++ app.js
@@ -1,60 +1,78 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+  // Hide resaults
+  document.getElementById('results').style.display = 'none';
+  // Show Loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 1500);
+
+  e.preventDefault();
+});
 
 //Calculate results
-function calculateResults(e){
-    // UI variables
-    const amount = document.getElementById('amount');
-    const interest = document.getElementById('interest');
-    const years = document.getElementById('years');
-    const monthlyPayment = document.getElementById('monthly-payment');
-    const totalPayment = document.getElementById('total-payment');
-    const totalInterest = document.getElementById('total-interest');
+function calculateResults() {
+  // UI variables
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('total-interest');
 
-    const principal = parseFloat(amount.value);
-    const caculatedInterest = parseFloat(interest.value) / 100 / 12;
-    const calculatedPayents = parseFloat(years.value) * 12;
+  const principal = parseFloat(amount.value);
+  const caculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayents = parseFloat(years.value) * 12;
 
-    //Compute monthly payments
-    const x = Math.pow(1 + caculatedInterest, calculatedPayents);
-    const monthly = (principal*x*caculatedInterest)/(x-1);
+  //Compute monthly payments
+  const x = Math.pow(1 + caculatedInterest, calculatedPayents);
+  const monthly = (principal * x * caculatedInterest) / (x - 1);
 
-    if(isFinite(monthly)){
-        monthlyPayment.value = monthly.toFixed(2);
-        totalPayment.value = (monthly * calculatedPayents).toFixed(2);
-        totalInterest.value = ((monthly * calculatedPayents)-principal).toFixed(2);
-    } else {
-        showError('Please check your numbers');
-    }
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayents).toFixed(2);
+    totalInterest.value = (monthly * calculatedPayents - principal).toFixed(2);
 
-    e.preventDefault();
+    //Show results
+    document.getElementById('results').style.display = 'block';
+
+    //Hide loader
+    document.getElementById('loading').style.display = 'none';
+  } else {
+    showError('Please check your numbers');
+  }
 }
 
 // Show Error
-function showError(error){
-    // Create a div
-    const errorDiv = document.createElement('div');
+function showError(error) {
+  //Hide results
+  document.getElementById('results').style.display = 'none';
 
-    // Get elements
-    const card = document.querySelector('.card');
-    const heading = document.querySelector('.heading');
+  //Hide loader
+  document.getElementById('loading').style.display = 'none';
+  // Create a div
+  const errorDiv = document.createElement('div');
 
-    // Add class
-    errorDiv.className = 'alert alert-danger';
+  // Get elements
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
 
-    // Create text node and append to div
+  // Add class
+  errorDiv.className = 'alert alert-danger';
 
-    errorDiv.appendChild(document.createTextNode(error));
+  // Create text node and append to div
 
-    // Insert error above heading
-    card.insertBefore(errorDiv, heading);
+  errorDiv.appendChild(document.createTextNode(error));
 
-    // Clear error afer 3 seconds
+  // Insert error above heading
+  card.insertBefore(errorDiv, heading);
 
-    setTimeout(clearError, 3000);
+  // Clear error afer 3 seconds
+
+  setTimeout(clearError, 3000);
 }
 
 //Clear error
-function clearError(){
-    document.querySelector('.alert').remove();
+function clearError() {
+  document.querySelector('.alert').remove();
 }
